2d13db2d5d4fbff83d26da542841cd6a70c53ac4
kimmoli <kimmo.lindholm@eke.fi>
2014-06-15 11:51:51 +0300

    Added buttons

--- Blinky.c
+++ Blinky.c
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include "STM32F4xx.h"
 #include "led.h"
+#include "keyboard.h"
 
 volatile uint32_t msTicks;                      /* counts 1ms timeTicks       */
 
@@ -43,6 +44,7 @@ int main (void) {
   int32_t led_num = LED_Num();
   int32_t num = -1; 
   int32_t dir =  1;
+	uint32_t keys = 0;
 
   SystemCoreClockUpdate();                      /* Get Core Clock Frequency   */
   if (SysTick_Config(SystemCoreClock / 1000)) { /* SysTick 1 msec interrupts  */
@@ -50,16 +52,27 @@ int main (void) {
   }
 
   LED_Initialize();
+	Keyboard_Initialize();
 
-  while(1) {                                    /* Loop forever               */
+  while(1)                                     /* Loop forever               */
+	{
+			keys = Keyboard_GetKeys();
+		
+			if ( keys > 0)
+			{
+				if (keys & 1) LED_On(0);
+				if (keys & 2) LED_On(1);
+			}
+			else
+			{
+				num += dir;
+				if (num == led_num) { dir = -1; num =  led_num-1; } 
+				else if   (num < 0) { dir =  1; num =  0;         }
 
-      num += dir;
-      if (num == led_num) { dir = -1; num =  led_num-1; } 
-      else if   (num < 0) { dir =  1; num =  0;         }
-
-      LED_On (num);
-      Delay( 50);                               /* Delay 50ms                 */
-      LED_Off(num);
-      Delay(200);                               /* Delay 200ms                */
+				LED_On (num);
+				Delay( 50);                               /* Delay 50ms                 */
+				LED_Off(num);
+				Delay(200);                               /* Delay 200ms                */
+			}
   }
 }
---
+++ Keyboard.c
@@ -0,0 +1,91 @@
+/*-----------------------------------------------------------------------------
+ * Name:    Keyboard.c
+ * Purpose: Low level Keyboard functions
+ *-----------------------------------------------------------------------------
+ * This file is part of the uVision/ARM development tools.
+ * This software may only be used under the terms of a valid, current,
+ * end user licence from KEIL for a compatible version of KEIL software
+ * development tools. Nothing else gives you the right to use this software.
+ *
+ * This software is supplied "AS IS" without warranties of any kind.
+ *
+ * Copyright (c) 2004-2013 KEIL - An ARM Company. All rights reserved.
+ *----------------------------------------------------------------------------*/
+
+#include "Keyboard.h"
+#include "GPIO_STM32F4xx.h"
+
+const GPIO_PIN_ID Pin_Key[] = {
+  { GPIOB, 14 },
+  { GPIOC,  3 }
+};
+
+#define NUM_KEYS (sizeof(Pin_Key)/sizeof(GPIO_PIN_ID))
+
+
+/*-----------------------------------------------------------------------------
+ *      Keyboard_Initialize:  Initialize keyboard/buttons
+ *
+ * Parameters: (none)
+ * Return:     (none)
+ *----------------------------------------------------------------------------*/
+void Keyboard_Initialize (void) {
+	
+  uint32_t n;
+
+  /* Configure pins: Input Mode (25 MHz) */
+  for (n = 0; n < NUM_KEYS; n++) 
+	{
+    GPIO_PortClock   (Pin_Key[n].port, true);
+    GPIO_PinWrite    (Pin_Key[n].port, Pin_Key[n].num, 0);
+    GPIO_PinConfigure(Pin_Key[n].port, Pin_Key[n].num,
+                    GPIO_MODE_INPUT,
+                    GPIO_OUTPUT_PUSH_PULL,
+                    GPIO_OUTPUT_SPEED_25MHz,
+                    GPIO_NO_PULL_UP_DOWN);  
+	}	
+}
+
+
+/*-----------------------------------------------------------------------------
+ *      Keyboard_Uninitialize:  Uninitialize keyboard/buttons
+ *
+ * Parameters: (none)
+ * Return:     (none)
+ *----------------------------------------------------------------------------*/
+void Keyboard_Uninitialize (void) {
+}
+
+
+/*-----------------------------------------------------------------------------
+ *      Keyboard_GetKeys:  Get keyboard state
+ *
+ * Parameters: (none)
+ * Return:      Keys bitmask
+ *----------------------------------------------------------------------------*/
+uint32_t Keyboard_GetKeys (void) {
+  /* Read board keyboard inputs */
+  uint32_t val = 0;
+	uint32_t n;
+
+  /* Configure pins: Input Mode (25 MHz) */
+  for (n = 0; n < NUM_KEYS; n++) 
+	{
+  if (GPIO_PinRead(Pin_Key[n].port, Pin_Key[n].num) != 0) {
+    /* User button */
+    val |= 1<<n;
+		}
+	}
+  return (val);
+}
+
+
+/*-----------------------------------------------------------------------------
+ *      Keyboard_NumKeys:  Get number of available keys
+ *
+ * Parameters: (none)
+ * Return:      number of keys
+ *----------------------------------------------------------------------------*/
+uint32_t Keyboard_NumKeys (void) {
+  return (NUM_KEYS);
+}
